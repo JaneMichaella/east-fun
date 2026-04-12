bfa46d6e72a0ebcc6c36dc54db6b392df704c65a
JaneMichaella <janemichaella@163.com>
2026-04-12 23:28:10 +0800

    fix: correct BASE_URL path joining and remove trailing slash from base config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://janemichaella.github.io',
-  base: '/east-fun/',
+  base: 'east-fun',
 });
